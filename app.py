170e3adb18dacfb315a778322cd9c370d98925b2
SalmaFetji <salma_fetji@hotmail.fr>
2022-10-31 15:44:28 +0100

    first

--- app.py
+++ app.py
@@ -5,6 +5,3 @@ import pandas as pd
 
 st.write('Hello, *World DE MERDE!* :sunglasses:')
 
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Seattle Energy", page_icon=":high_brightness:", layout="wide")
-
