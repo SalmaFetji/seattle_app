2e290561163a46523280a124d108d49e5a834948
SalmaFetji <salma_fetji@hotmail.fr>
2022-11-23 12:18:12 +0100

    first

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import streamlit as st
 import pandas as pd
 
 
-st.write('Hello, *World DE MERDE!* :sunglasses:')
+st.write('Hello, *World* :sunglasses:')
 
 
-st.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: red;'>Seattle energy</h1>", unsafe_allow_html=True)
+
+
+option = st.selectbox(
+    'What is your property Use ?',
+    ('Low-Rise Multifamily', 'Mixed Use Property', 'Senior Care Community', "University"))
+
 
