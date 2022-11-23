f51334d59bc57ea07321af1be481a45505a9370f
SalmaFetji <salma_fetji@hotmail.fr>
2022-11-23 19:27:48 +0100

    first

--- app.py
+++ app.py
@@ -65,13 +65,13 @@ def main():
 
 
 	#d = {'NaturalGas(kBtu)': [NaturalGas], 'SteamUse(kBtu)': [SteamUse],'GHGEmissionsIntensity': [GHGEmissionsIntensity],'SourceEUI(kBtu/sf)': [SourceEUI], 'PrimaryPropertyType': [PrimaryPropertyType], 'NumberofFloors': [NumberofFloors], 'harvesine_distance': [harvesine_distance], 'BuildingAge': [BuildingAge]}   
-	d = {'NaturalGas(kBtu)': [1], 'SteamUse(kBtu)': [1],'GHGEmissionsIntensity': [1],'SourceEUI(kBtu/sf)': [1], 'PrimaryPropertyType': [0], 'NumberofFloors': [1], 'harvesine_distance': [1], 'BuildingAge': [1]}
-	X_test = pd.DataFrame(data=d)
+	dd = {'NaturalGas(kBtu)': [1], 'SteamUse(kBtu)': [1],'GHGEmissionsIntensity': [1],'SourceEUI(kBtu/sf)': [1], 'PrimaryPropertyType': [0], 'NumberofFloors': [1], 'harvesine_distance': [1], 'BuildingAge': [1]}
+	X_test = pd.DataFrame(data=dd)
 
 
 	#st.dataframe(X_test)
 
-	#y_pred = model.predict(X_test)
+	y_pred = model.predict(X_test)
 
 
 
