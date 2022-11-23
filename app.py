e6716e2ff03e03ab1e37b90bb5f4ca101b8ef61a
SalmaFetji <salma_fetji@hotmail.fr>
2022-11-23 12:29:11 +0100

    first

--- app.py
+++ app.py
@@ -13,3 +13,17 @@ option = st.selectbox(
     ('Low-Rise Multifamily', 'Mixed Use Property', 'Senior Care Community', "University"))
 
 
+number = st.number_input('Number of floors')
+st.write('The current number is ', number)
+
+number2 = st.number_input('Age of the building')
+st.write('The current number is ', number2)
+
+number3 = st.number_input('Distance from Seattle Center')
+st.write('The current number is ', number3)
+
+
+if st.button('Predict Consumption'):
+    st.write('The building consumption is ')
+else:
+    st.write('Hit the button')
