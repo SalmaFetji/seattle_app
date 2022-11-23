b6f15422e8a244648dcc2b3c7fdad312a5b0f483
SalmaFetji <salma_fetji@hotmail.fr>
2022-11-23 12:53:09 +0100

    first

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import pickle
 
 
 st.write('Hello, *World* :sunglasses:')
@@ -14,6 +15,9 @@ with colT2:
 
 
 st.markdown("<h1 style='text-align: center; color: blue;'>CO2 Emissions Predictions</h1>", unsafe_allow_html=True)
+
+# # welcome info
+    st.info(":high_brightness: Welcome here !  caracteristiques batiments")
 
 
 option = st.selectbox(
@@ -29,6 +33,25 @@ st.write('The current number is ', number2)
 
 number3 = st.number_input('Distance from Seattle Center')
 st.write('The current number is ', number3)
+
+# # welcome info
+    st.info(":high_brightness: Welcome here !  Hop,  you can predict the energy use by the building in Seattle and the GHG emissions by year")
+
+number4 = st.number_input('NaturalGas(kBtu)')
+st.write('The current number is ', number4)
+
+number5 = st.number_input('SteamUse(kBtu)')
+st.write('The current number is ', number5)
+
+number6 = st.number_input('GHGEmissionsIntensity')
+st.write('The current number is ', number6)
+
+number7 = st.number_input('SourceEUI(kBtu/sf)')
+st.write('The current number is ', number7)
+
+        
+    with open('rf_app_pickle' , 'rb') as f:
+        model= pickle.load(f)
 
 
 if st.button('Predict Consumption'):
