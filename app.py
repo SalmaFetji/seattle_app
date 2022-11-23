0951e33f97e2ba3d57b15119c441413be8be850d
SalmaFetji <salma_fetji@hotmail.fr>
2022-11-23 13:26:12 +0100

    first

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 import pickle
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 
 st.write('Hello, *World* :sunglasses:')
 
